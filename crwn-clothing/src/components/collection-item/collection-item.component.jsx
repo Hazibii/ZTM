ba81650fbe8be8601d342792088b287384787afe
Hasib Sarvari <tkdparallax@gmail.com>
2020-07-09 10:35:59 +0800

    adding header component and linkgs to home and shop and contact

--- crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -2,22 +2,19 @@ import React from 'react';
 
 import './collection-item.styles.scss';
 
-
-const CollectionItem = ({id, name, price, imageUrl})=>(
-    <div className="collection-item">
-        <div 
-        className="image"
-        style={{
-            background: `url(${imageUrl})`}}
-        />
-        <div className="collection-footer">
-            <spam className="name">{name}</spam>
-            <span className="price">{price}</span>
-        </div>
-        
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className='collection-item'>
+    <div
+      className='image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
+  </div>
 );
 
-
-
-export default CollectionItem; 
+export default CollectionItem;
