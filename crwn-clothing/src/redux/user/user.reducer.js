cfc3ee73c8dc206f22acd697d15e1fe1d540cb4d
Hazibii <tkdparallax@gmail.com>
2020-07-30 10:35:01 +0800

    added redux, and implemented userReducer adn userActions to header and app components

--- crwn-clothing/src/redux/user/user.reducer.js
+++ crwn-clothing/src/redux/user/user.reducer.js
@@ -3,14 +3,14 @@ const INITIAL_STATE = {
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
-  switch (action) {
+  switch (action.type) {
     case "SET_CURRENT_USER":
       return {
         ...state,
         currentUser: action.payload,
       };
     default:
-      return;
+      return state;
   }
 };
 
